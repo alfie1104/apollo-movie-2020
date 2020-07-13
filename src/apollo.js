c2bf1dfdc5eb6e804fef7fbf47f874029749ff91
peter_alfie <alfie1104@gmail.com>
2020-07-14 08:26:26 +0900

    ApolloClient setting is done

---
+++ src/apollo.js
@@ -0,0 +1,12 @@
+import ApolloClient from "apollo-boost";
+/*
+    Apollo-Boost는 graphQL yoga처럼 모든 것을 다 설정해둔 package임(클라이언트 사이드. 서버사이드는 graphQL Yoga사용)
+    한편, Apollo를 이용하면 GraphQL 요청을 보낼 때 별도로 POST 메시지 생성을 해줄 필요가 없음
+    원래는 AXIOS나 FETCH 메소드로 POST방식으로 graphQL 쿼리나 뮤테이션을 보내야함
+*/
+
+const client = new ApolloClient({
+  uri: "https://movieql.now.sh",
+});
+
+export default client;
